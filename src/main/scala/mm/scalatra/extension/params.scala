f2362128f0d3365dbef45500ac1cac51f02893a3
Massimiliano Mazzarolo <massimiliano.mazzarolo@gmail.com>
2012-01-11 23:43:40 +0100

    Revert e5f58ffe9f667361d46062d54e2f00c5f00e43eb^..HEAD

--- src/main/scala/mm/scalatra/extension/params.scala
+++ src/main/scala/mm/scalatra/extension/params.scala
@@ -1,23 +1,41 @@
 package mm.scalatra.extension
 
 import mm.scalatra.common.conversions.{TypeConverter, DefaultImplicitConversions}
+import java.util.Date
+import org.scalatra.util.{MultiMap, MapWithIndifferentAccess, MultiMapHeadView}
 
-
+/**
+ * Add some implicits
+ */
 trait ScalatraParamsImplicits {
 
   self: DefaultImplicitConversions =>
 
   type ParamsType = MultiMapHeadView[String, String] with MapWithIndifferentAccess[String]
 
-  sealed case class TypedParams(params: ParamsType) {
+  type MultiParamsType = MultiMap
+
+
+  sealed class TypedParams(params: ParamsType) {
+
     def getAs[T <: Any](name: String)(implicit tc: TypeConverter[T]): Option[T] = params.get(name).flatMap(tc.convert(_))
+
+    def getAsDate(nameAndFormat: (String, String)) : Option[Date] = getAs(nameAndFormat._1)(stringToDate(nameAndFormat._2))
+
+    def getAsSeq[T <: Any](name: String)(implicit tc: TypeConverter[T]): Option[Seq[T]] = getAs(name)(stringToSeq(tc))
+  }
+
+  sealed class TypedMultiParams(multiParams: MultiParamsType) {
+
+    def getAs[T <: Any](name: String)(implicit tc: TypeConverter[T]): Option[Seq[T]] = multiParams.get(name) map { s =>
+      s.flatMap(tc.convert(_))
+    }
+
   }
 
   implicit def toTypedParams(params: ParamsType) = new TypedParams(params)
 
+  implicit def toTypedMultiParams(params: MultiParamsType) = new TypedMultiParams(params)
 }
 
-object Params extends ScalatraParamsImplicits with DefaultImplicitConversions {
-
-
-}
+object Params extends ScalatraParamsImplicits with DefaultImplicitConversions
